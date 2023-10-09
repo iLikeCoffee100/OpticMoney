b2b99b63ed7df8609c511dec0dde402b886ea25a
Admin <Admin@DESKTOP-E7GT8H3>
2023-10-09 04:23:15 -0700

    Add project files.

---
+++ OpticMoney/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OpticMoney
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
